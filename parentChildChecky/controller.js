427fa2147a0c0a6ee6992065b388ffc1cbadd456
GashVinsol <gaurav@vinsol.com>
2019-01-04 15:00:54 +0530

    reviewCorrection

--- parentChildChecky/controller.js
+++ parentChildChecky/controller.js
@@ -1,52 +1,58 @@
-const checkStructure = {
-    color: ['red', 'green', 'yellow', 'blue'],
-    drinks: ['coke', 'pepsi', 'dew'],
-    movies: ['adhm', 'detachment'],
-    bikes: ['harley', 'yamaha', 'r15']
+const DOMSelectors = {
+  containerSelector: 'container'
 }
 
-const checkMethods = {
-    initParentCheckboxes : function() {
-        var element = ``;
-        Object.keys(checkStructure).map((ele) => {
-            element += `<div class="checkbox"><label for=${ele}><input type="checkbox" id=${ele} value=${ele} /> ${ele}</label></div>${this.initChildCheckBoxes(ele)}`
-        })
-        document.getElementById('container').innerHTML = element;
-    
-        // Adds Event Listeners
-        this.initEventListeners();
-    },
-    changeCheck : function(ids, val) {
-        var doc = document;
-        ids.map(ele => {
-            doc.getElementById(ele).checked = val;
-        })
-    },
-    initChildCheckBoxes : function(prop) {
-        var childElement = ``;
-        checkStructure[prop].map((ele) => {
-            childElement += `<li><label for=${ele}><input type="checkbox" id=${ele} value=${ele} /> ${ele}</label></li>`
-        })
-        return `<ul id=${prop + 'list'} class="childList">${childElement}</ul>`
-    },
-    initEventListeners : function() {
-        Object.keys(checkStructure).map((ele) => {
-            document.addEventListener('click', (e)=>this.parentCheckChange(e), false)
-        })
-    },
-    parentCheckChange : function(e) {
-        let listId = e.target.value + 'list';
-        let doc = document;
-        if (e.target.checked) {
-            doc.getElementById(listId).style.display = 'block';
-            this.changeCheck(checkStructure[e.target.value], true);
-        } else {
-            doc.getElementById(e.target.value + 'list').style.display = 'none';
-            this.changeCheck(checkStructure[e.target.value], false);
-        }
-        doc.getElementById(listId).scrollIntoView();
+const checkBoxJson = {
+  color: ['red', 'green', 'yellow', 'blue'],
+  drinks: ['coke', 'pepsi', 'dew'],
+  movies: ['adhm', 'detachment'],
+  bikes: ['harley', 'yamaha', 'r15']
+}
+
+const checkBoxController = {
+  initParentCheckboxes: function () {
+    var element = ``;
+    Object.keys(checkBoxJson).map((ele) => {
+      element += `<div class="checkbox"><label for=${ele}><input type="checkbox" id=${ele} value=${ele} /> ${ele}</label></div>${this.initChildCheckBoxes(ele)}`
+    })
+    document.getElementById(DOMSelectors.containerSelector).innerHTML = element;
+
+    // Adds Parent Checkboxes Event Listeners
+    this.initParentEventListeners();
+  },
+  changeAllChildCheckboxes: function (ids, val) {
+    var doc = document;
+    ids.map(ele => {
+      doc.getElementById(ele).checked = val;
+    })
+  },
+  initChildCheckBoxes: function (prop) {
+    var childElement = ``;
+    checkBoxJson[prop].map((ele) => {
+      childElement += `<li><label for=${ele}><input type="checkbox" id=${ele} value=${ele} /> ${ele}</label></li>`
+    })
+    return `<ul id=${prop + 'List'} class="childList">${childElement}</ul>`
+  },
+  initParentEventListeners: function () {
+    Object.keys(checkBoxJson).map(() => {
+      document.addEventListener('click', (e) => this.parentCheckboxHandler(e), false);
+    })
+  },
+  parentCheckboxHandler: function (e) {
+    let { checked, value } = e.target;
+    let listId = value + 'List';
+    let doc = document;
+    if (checked) {
+      doc.getElementById(listId).style.display = 'block';
+      this.changeAllChildCheckboxes(checkBoxJson[value], true);
+    } else {
+      doc.getElementById(listId).style.display = 'none';
+      this.changeAllChildCheckboxes(checkBoxJson[value], false);
     }
+    doc.getElementById(listId).scrollIntoView();
+  }
 }
 
-
-checkMethods.initParentCheckboxes();
+  (function () {
+    checkBoxController.initParentCheckboxes();
+  })();
