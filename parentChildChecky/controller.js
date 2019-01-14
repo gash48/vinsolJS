7ec285b6033b4954207c430c4cabc0efcb448a4c
GashVinsol <gaurav@vinsol.com>
2019-01-14 13:01:21 +0530

    parentChildCheckBoxes Review COrrection

--- parentChildChecky/controller.js
+++ parentChildChecky/controller.js
@@ -1,11 +1,17 @@
+/* eslint-disable one-var-declaration-per-line */
+/* eslint-disable one-var */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable object-curly-newline */
+/* eslint-disable max-len */
 /* eslint-disable no-new */
 /* eslint-disable func-names */
 /* eslint-disable quote-props */
-/* eslint-disable prefer-const */
 /* eslint-disable array-callback-return */
 /* eslint-disable class-methods-use-this */
 const DOMSelectors = {
   containerSelector: 'container',
+  checkBoxClass: 'checkBoxes',
+  childCheckBoxesList: 'childList',
 };
 
 const checkBoxValuesJson = {
@@ -24,14 +30,12 @@ class CheckBoxController {
   }
 
   initParentCheckboxes() {
-    let tempDocFragment = document.createDocumentFragment();
+    const tempDocFragment = document.createDocumentFragment();
     Object.keys(this.checkBoxValues).map((ele) => {
-      let checkBoxContainer = this.createSetElement('div', { 'class': 'checkbox' });
-      let label = this.createSetElement('label', { 'for': ele });
-      label.textContent = ele;
-      let checkBox = this.createSetElement('input', { 'type': 'checkbox', 'id': ele, 'value': ele });
-      checkBoxContainer.appendChild(checkBox);
-      checkBoxContainer.appendChild(label);
+      const checkBoxContainer = this.createSetElement('div', { 'class': 'checkbox' }), label = this.createSetElement('label', { 'for': ele }, ele),
+        checkBox = this.createSetElement('input', { 'type': 'checkbox', 'id': ele, 'value': ele, class: this.DOMSelectors.checkBoxClass });
+
+      checkBoxContainer.appendChild(label).appendChild(checkBox);
       checkBoxContainer.appendChild(this.initChildCheckBoxes(ele));
       tempDocFragment.appendChild(checkBoxContainer);
     });
@@ -48,26 +52,24 @@ class CheckBoxController {
   }
 
   initChildCheckBoxes(prop) {
-    let tempDocFragment = document.createDocumentFragment();
-    let listHead = this.createSetElement('ul', { 'id': `${prop}List`, 'class': 'childList' });
+    const tempDocFragment = document.createDocumentFragment(), listHead = this.createSetElement('ul', { 'id': `${prop}List`, 'class': this.DOMSelectors.childCheckBoxesList });
     this.checkBoxValues[prop].map((ele) => {
-      let childList = this.createSetElement('li');
-      let label = this.createSetElement('label', { 'for': ele });
-      label.textContent = ele;
-      let checkBox = this.createSetElement('input', { 'type': 'checkbox', 'id': ele, 'value': ele });
-      childList.appendChild(checkBox);
-      childList.appendChild(label);
+      const childList = this.createSetElement('li');
+      const label = this.createSetElement('label', { 'for': ele }, ele);
+      const checkBox = this.createSetElement('input', { 'type': 'checkbox', class: this.DOMSelectors.checkBoxClass, 'id': ele, 'value': ele });
+      childList.appendChild(label).appendChild(checkBox);
       tempDocFragment.appendChild(childList);
     });
     listHead.appendChild(tempDocFragment);
     return listHead;
   }
 
-  createSetElement(element, attributes = {}) {
-    let createdElement = document.createElement(element);
+  createSetElement(element, attributes = {}, innerText = '') {
+    const createdElement = document.createElement(element);
     Object.keys(attributes).map((key) => {
       createdElement.setAttribute(key, attributes[key]);
     });
+    createdElement.textContent = innerText;
     return createdElement;
   }
 
@@ -78,8 +80,7 @@ class CheckBoxController {
   }
 
   parentCheckboxHandler(e) {
-    const { checked, value } = e.target;
-    const listId = `${value}List`;
+    const { checked, value } = e.target, listId = `${value}List`;
     document.getElementById(listId).style.display = checked ? 'block' : 'none';
     this.switchChildCheckboxes(this.checkBoxValues[value], checked);
     document.getElementById(listId).scrollIntoView(false);
